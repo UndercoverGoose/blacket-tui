376c75fa667cfc7392ebabe689a13f423cc33ec0
goose <53491740+UndercoverGoose@users.noreply.github.com>
2024-07-31 22:03:07 -0400

    fix claim incorrect reset bug

--- src/util/claim.ts
+++ src/util/claim.ts
@@ -3,14 +3,16 @@ function get_reset_time(last_claim_date: Date): number {
   reset_date.setHours(20);
   reset_date.setMinutes(0);
   reset_date.setSeconds(0);
-  return reset_date.getTime();
+  let time = reset_date.getTime();
+  if (time < last_claim_date.getTime()) time += 86400000;
+  return time;
 }
 
 export function can_claim(last_claim_date: Date): boolean {
   const now = Date.now();
   const last = last_claim_date.getTime();
   const reset = get_reset_time(last_claim_date);
-  return (reset < now && last < reset) || (now - last > 86400000);
+  return (reset < now && last < reset) || now - last > 86400000;
 }
 export function claim_in_ms(last_claim_date: Date): number {
   if (can_claim(last_claim_date)) return 0;
