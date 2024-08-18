93756252de8741565a32839f152dd888f1448ad1
goose <53491740+UndercoverGoose@users.noreply.github.com>
2024-08-18 02:45:55 -0400

    support jumping

--- src/context/statistics.ts
+++ src/context/statistics.ts
@@ -1,198 +1,140 @@
-import { Text } from '@lib/tui';
 import v1 from '@lib/api';
 import Color from '@lib/color';
-import { Select, Input, Searchable } from '@component/.';
+import { Select, Input } from '@component/.';
 import { can_claim, claim_in_formatted } from '@util/claim';
-import type { UserForeign } from '@lib/api/src/v1/user';
-import type { User as UserFriend } from '@lib/api/src/v1/friends';
-import type { Data } from '@lib/api/src/v1/data';
+import type { User, UserForeign } from '@lib/api/src/v1/user';
 import type { State } from '@ctx/state';
 
-const status_text = new Text(0, 0, '');
-const stat_text = new Text(0, 6, '');
-const root_select_choices = ['-> View Stats ', '-> Trade Player ', '-> Manage Friends ', '-> Claim Daily Reward '];
-const root_select = new Select('Select an action to perform:', root_select_choices, {
-  disabled_indexes: [1, 4],
+const root_select_options = ['-> View Another Player ', '-> Manage Friends ', '-> Claim Daily Reward '];
+const root_select = new Select('Select an action to perform:', root_select_options, {
+  disabled_indexes: [3],
 });
-const user_input = new Input('Enter the username or ID of the player:', {
-  placeholder: 'Username or ID',
+const user_input = new Input('Enter the username or id of the player you want to view:', {});
+const foreign_select_options = ['-> Send Trade ', '-> View Blooks ', '-> View Clan '];
+const foreign_select = new Select('Select an action to perform:', foreign_select_options, {
+  disabled_indexes: [0, 3],
 });
-const friend_search = new Searchable('Select a friend for more options:', []);
-const friend_select = new Select('Select an action to perform:', ['-> View Stats ', '-> Send Trade Request ', '-> Unfriend ', '-> Block '], {
-  disabled_indexes: [1, 2, 3],
-  header_func: s => s,
-});
-
-let cached_data: Data | null = null;
-
-function set_stat_text(user: UserForeign) {
-  stat_text.text = Color.green(
-    Color.underline('User'),
-    ': ',
-    Color.hex(user.color, user.username),
-    !user.clan ? '' : Color.hex(user.clan.color, ' [' + user.clan.name + ']'),
-    Color.bright_black(` #${user.id}\n`),
-    Color.underline('Joined'),
-    `: ${new Date(user.created * 1000).toLocaleString()}\n`,
-    Color.underline('Last Seen'),
-    `: ${new Date(user.modified * 1000).toLocaleString()}\n`,
-    Color.underline('Tokens'),
-    ': ',
-    Color.bold(Color.yellow(user.tokens.toLocaleString())),
-    '\n',
-    Color.underline('Experience'),
-    ': ',
-    Color.bold(Color.yellow(user.exp.toLocaleString())),
-    '\n',
-    Color.underline('Unlocks'),
-    ': ',
-    Color.bold(Color.yellow(Object.keys(user.blooks).length.toString())) + `/` + Color.yellow(Object.keys(cached_data?.blooks ?? {}).length.toString()),
-    '\n',
-    Color.underline('Packs Opened'),
-    ': ',
-    Color.bold(Color.yellow(user.misc.opened.toLocaleString())),
-    '\n',
-    Color.underline('Messages Sent'),
-    ': ',
-    Color.bold(Color.yellow(user.misc.messages.toLocaleString())),
-    '\n'
-  );
-  if (user.clan) {
-    root_select_choices[4] = `${'\n'.repeat(10)}[4] View Clan `;
-  } else {
-    delete root_select_choices[4];
-  }
-  root_select.set_options(root_select_choices);
-}
 
 export const states = {
   /**
-   * The root state for the statistics context.
-   * @param state The current state.
+   * Root state for the statistics section. This fetches and displays the authenticated user's statistics.
+   * @param state The current state object.
    */
   root: async (state: State): Promise<void> => {
-    status_text.text = Color.yellow('Fetching statistics...');
-    state.terminal.push(status_text);
     const user_res = await v1.user(state.token);
-    if (!cached_data) {
-      const data_res = await v1.data(true);
-      if (data_res.error) {
-        state.notif_section.push_error(data_res.reason);
-        state.terminal.pop(status_text);
-        return;
+    if (user_res.error) state.notif_section.push_error(user_res.reason);
+    const text = user_res.error ? Color.red('An error occurred.') : await states.generate_stat_text(user_res.user);
+    if (!user_res.error) {
+      const date = new Date(+(user_res.user as User).claimed * 1000);
+      if (can_claim(date)) {
+        root_select_options[2] = '-> Claim Daily Reward ';
+        root_select.set_disabled_indexes([3]);
+      } else {
+        root_select_options[2] = `-> Claim Daily Reward (in ${claim_in_formatted(date)})`;
+        root_select.set_disabled_indexes([2, 3]);
       }
-      cached_data = data_res.data;
+    } else {
+      root_select_options[2] = '-> Claim Daily Reward ';
+      root_select.set_disabled_indexes([2, 3]);
     }
-    if (user_res.error) {
-      state.notif_section.push_error(user_res.reason);
-      state.terminal.pop(status_text);
-      return;
-    }
-    if (user_res.is_foreign) throw new Error('User is not allowed to be `is_foreign`');
-    status_text.text = '';
-    const user = user_res.user;
-    set_stat_text(user);
-    if (can_claim(new Date(+user.claimed * 1000))) root_select.set_disabled_indexes([1, 4]);
-    else {
-      root_select_choices[3] = `[3] Claim Daily Reward (${claim_in_formatted(new Date(+user.claimed * 1000))})`;
-      root_select.set_disabled_indexes([1, 3, 4]);
-    }
+    root_select_options[3] = text;
     while (true) {
-      state.terminal.push(stat_text);
-      const root_select_index = await root_select.response_bind(state.terminal);
-      state.terminal.pop(stat_text);
-      switch (root_select_index) {
-        case -1:
+      switch (await root_select.response_bind(state.terminal)) {
+        case -1: {
           return;
+        }
         case 0: {
-          await states.set_visible_stats(state);
+          await states.search_foreign(state);
           break;
         }
         case 1: {
           break;
         }
         case 2: {
-          const friend = await states.select_friend(state);
-          if (!friend) break;
-          await states.friend_actions(state, friend);
-          break;
-        }
-        case 3: {
-          const claim_res = await v1.claim(state.token);
-          if (claim_res.error) state.notif_section.push(Color.red('Daily reward: ', claim_res.reason));
-          else {
-            state.notif_section.push(Color.green('Claimed ', Color.bold(claim_res.tokens.toLocaleString()), ' tokens from daily reward.'));
-            state.tokens.add_tokens(claim_res.tokens);
-            root_select.set_disabled_indexes([1, 3, 4]);
-          }
-          break;
-        }
-        case 4: {
+          const worked = await states.claim_reward(state);
+          if (worked) return await states.root(state);
           break;
         }
       }
     }
   },
   /**
-   * Set the visible statistics to a specific user.
-   * @param state The current state.
-   * @param user_id The user ID to set the statistics to.
+   * Generates a formatted string of a user's statistics.
+   * @param user The user object to generate the statistics for.
+   * @returns A formatted string of the user's statistics.
    */
-  set_visible_stats: async (state: State, user_id?: string | number): Promise<void> => {
+  generate_stat_text: async (user: UserForeign): Promise<string> => {
+    const data = await v1.data();
+    if (data.error) return Color.red('An error occurred.');
+    const unlocks = Object.keys(user.blooks).length;
+    const max_unlocks = Object.keys(data.data.blooks).length;
+    return [
+      Color.join(
+        Color.reset(Color.bold('[', user.role, '] ')),
+        Color.hex(user.color, user.username),
+        !user.clan ? ' ' : Color.hex(user.clan.color, ' [', user.clan.name, '] '),
+        Color.bright_black('#', user.id.toString())
+      ),
+      Color.green(Color.bold('Badges: '), user.badges.join(', ')),
+      Color.green(Color.bold('Mutual Friends: '), Color.yellow(user.friends.length.toString())),
+      Color.green(Color.bold('Joined: '), new Date(user.created * 1000).toLocaleString()),
+      Color.green(Color.bold('Last Seen: '), new Date(user.modified * 1000).toLocaleString()),
+      Color.bold(Color.green('Tokens: '), Color.yellow(user.tokens.toLocaleString())),
+      Color.bold(Color.green('Experience: '), Color.yellow(user.exp.toLocaleString())),
+      Color.green(
+        Color.bold('Unlocks: ', Color.yellow(unlocks.toString())),
+        '/',
+        Color.yellow(max_unlocks.toString()),
+        ' ',
+        ((unlocks / max_unlocks) * 100).toFixed(1),
+        '%'
+      ),
+      Color.bold(Color.green('Packs Opened: '), Color.yellow(user.misc.opened.toLocaleString())),
+      Color.bold(Color.green('Messages Sent: '), Color.yellow(user.misc.messages.toLocaleString())),
+    ].join('\n');
+  },
+  /**
+   * Claims the daily reward for the authenticated user.
+   * @param state The current state object.
+   * @returns Whether the claim was successful.
+   */
+  claim_reward: async (state: State): Promise<boolean> => {
+    const claim_res = await v1.claim(state.token);
+    if (claim_res.error) {
+      state.notif_section.push_error(claim_res.reason);
+      return false;
+    }
+    state.notif_section.push_success(`Received ${Color.bold(claim_res.tokens.toLocaleString())} tokens.`);
+    state.tokens.add_tokens(claim_res.tokens);
+    return true;
+  },
+  /**
+   * Prompts to search for another user and then views their stats.
+   * @param state The current state object.
+   */
+  search_foreign: async (state: State): Promise<void> => {
     user_input.set_value('');
-    const user = user_id ?? (await user_input.response_bind(state.terminal));
-    if (user === '') return;
+    const input_res = await user_input.response_bind(state.terminal);
+    if (input_res === '') return;
+    await states.view_foreign(state, input_res);
+  },
+  /**
+   * Views the statistics of a foreign user.
+   * @param state The current state object.
+   * @param user The name or id of a user to view the statistics of.
+   */
+  view_foreign: async (state: State, user: string | number): Promise<void> => {
     const user_res = await v1.user(state.token, user);
     if (user_res.error) {
       state.notif_section.push_error(user_res.reason);
       return;
     }
-    set_stat_text(user_res.user);
-  },
-  /**
-   * Select a friend to perform an action on.
-   * @param state The current state.
-   * @returns The friend user object.
-   */
-  select_friend: async (state: State): Promise<UserFriend | void> => {
-    const friends_res = await v1.friends(state.token);
-    if (friends_res.error) return state.notif_section.push_error(friends_res.reason);
-    const friends_map = friends_res.friends.map(f => f.username);
-    friend_search.mutate_func = (friend_name: string) => {
-      const friend = friends_res.friends.find(f => f.username === friend_name)!;
-      return Color.hex(friend.color, '[', friend.role, '] ', friend.username);
-    };
-    friend_search.set_choices(friends_map);
-    const friend_index = await friend_search.response_bind(state.terminal);
-    if (friend_index === -1) return;
-    const selected_friend = friends_res.friends[friend_index];
-    return selected_friend;
-  },
-  /**
-   * Perform an action on a friend.
-   * @param state The current state.
-   * @param selected_friend The selected friend to perform an action on.
-   */
-  friend_actions: async (state: State, selected_friend: UserFriend): Promise<void> => {
-    friend_select.set_question(
-      Color.join(
-        Color.green(Color.underline('Select an action to perform on'), ' '),
-        Color.hex(selected_friend.color, '[', selected_friend.role, '] ', selected_friend.username)
-      )
-    );
-    main: while (true) {
-      switch (await friend_select.response_bind(state.terminal)) {
+    const text = await states.generate_stat_text(user_res.user);
+    foreign_select_options[3] = text;
+    while (true) {
+      switch (await foreign_select.response_bind(state.terminal)) {
         case -1: {
-          break main;
-        }
-        case 0: {
-          const user_res = await v1.user(state.token, selected_friend.id);
-          if (user_res.error) {
-            state.notif_section.push_error(user_res.reason);
-            break;
-          }
-          set_stat_text(user_res.user);
-          break main;
+          return;
         }
       }
     }
